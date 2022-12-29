3f6de68f7be9f6c43c21593a27e77172cbae7b8b
Jacob Gorton <jgorton200@gmail.com>
2022-12-29 13:16:59 -0600

    updated fetch game

--- application/data/fetchGame.js
+++ application/data/fetchGame.js
@@ -1,18 +1,19 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-const AWS = require("aws-sdk");
-const documentClient = new AWS.DynamoDB.DocumentClient();
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb"
+
+const dbclient = new DynamoDBClient();
 
 const fetchGame = async gameId => {
   const params = {
     TableName: "turn-based-game",
     Key: {
-      gameId: gameId
+      gameId: {'S': gameId}
     }
   };
 
   try {
-    const game = await documentClient.get(params).promise();
+    const game = await dbclient.send(new GetItemCommand(params));
     return game.Item;
   } catch (error) {
     console.log("Error fetching game: ", error.message);
